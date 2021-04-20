24dcbb86645d12f1a5ce1d383a2d80df17386de3
cmurphey8 <53098445+cmurphey8@users.noreply.github.com>
2021-04-20 08:38:28 -0400

    Update Tetra.java

--- Tetra.java
+++ Tetra.java
@@ -3,7 +3,7 @@
  *  Execution:    java Tetra
  *  Dependencies: StdDraw.java Shape.java Tetroid*.java
  * 
- *  Usage:  Move blocks with keys: a (left), a (right), s (down), w (rotate), space (slam down)
+ *  Usage:  Move blocks with keys: a (left), a (right), s (speed down), w (rotate), space (slam down)
  * 
  *  PART 1: Complete the TetraSet Class -> complete the reduce() method
  *
